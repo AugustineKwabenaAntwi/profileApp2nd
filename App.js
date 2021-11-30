d8a8041e7379a4940c2cbb7f8bb9e22d6fb32b11
AugustineKwabenaAntwi <antwigoodness@gmail.com>
2021-11-30 16:38:45 +0000

    1st update

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image,Button} from 'react-native';
+import { StyleSheet, Text, View,Image,Pressable} from 'react-native';
 import profile2 from "./assets/profile2.jpg"
 
-export default function App() {
+export default function App(props) {
+  const {title ="UPDATE PROFILE"}=props;
   return (
     <View style={styles.container}>
 
@@ -16,7 +17,7 @@ export default function App() {
         </View>
 
         <View style={styles.detailsCont}>
-          <Text style ={styles.label}>Email</Text>
+          <Text style ={styles.label}>Email Address</Text>
           <Text style ={styles.info}>dontknow@info.com</Text>
         </View>
 
@@ -32,10 +33,12 @@ export default function App() {
 
         <View style={styles.detailsCont}>
           <Text style ={styles.label}>Emergency Contact</Text>
-          <Text style ={styles.info}>i-can-never-be-a-+44</Text>
+          <Text style ={styles.info}>i-can-never-be-your-+44</Text>
         </View>
 
-        <Button title='UPDATE PROFILE'></Button>
+        <Pressable style={styles.btn}>
+          <Text style ={styles.btntxt}>{title}</Text>
+        </Pressable>
 
       </View>
     </View>
@@ -56,13 +59,37 @@ const styles = StyleSheet.create({
   },
   image:{
     borderRadius:100,
-    width:200,
-    height:200,
+    width:150,
+    height:150,
     alignSelf:"center",
     marginVertical:30
   },
   detailsCont:{
     marginHorizontal:30,
     marginVertical:20,
+  },
+  label:{
+    color:"#27dc"
+  },
+  info:{
+    fontWeight:"900"
+  },
+  btn:{
+    backgroundColor:"black",
+    alignSelf:"center",
+    justifyContent:"center",
+    marginVertical:30,
+    paddingVertical:15,
+    paddingHorizontal:80,
+    borderRadius:60,
+    elevation:2
+
+  },
+  btntxt:{
+    color:"white",
+    fontWeight:"bold",
+    fontSize:16,
+    letterSpacing:0.5,
+
   }
 });
